296ec088c65e365d7473d5393bfe781278ef2100
anirbanpramanik1999 <anirbanpramanik902@gmail.com>
2024-04-10 19:11:51 +0530

    my-app-2nd-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TextForm from './components/TextForm';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
+   const [mode,setmode]=useState(`light`)
+   const toggleMode=()=>{
+  
+     if (mode==='light') {
+      setmode('dark')
+      document.body.style.backgroundColor='#082d64'
+     }
+     else{
+      setmode('light')
+      document.body.style.backgroundColor='white'
+     }
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+
+  <Navbar title="Instaily Acedemy" about="About Instaily" link="New Registration" mode={mode} toggleMode={toggleMode} />
+  <Alert/>
+  <div className="container">
+  <TextForm heading="Write your Text " mode={mode}/>
+  </div>
+  
+  
+  </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,80 @@
+import React,{useState} from 'react'
+
+
+export default function TextForm(props) {
+    
+
+    const handleUpClick=()=>{
+        console.log("Uppercase is clicked");
+       // setText("SET TEXT clicked on handleUpClick")
+        setText(text.toUpperCase())
+    }
+
+    const handleLowClick=(event)=>{
+        setText(text.toLowerCase());
+
+    }
+
+    const handleClrClick=()=>{
+       const newText="";
+        setText(newText)
+    }
+
+    const handleCopy=()=>{
+      let text =document.getElementById("myBox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces=()=>{
+      let newText=text.split(/[ ]+/);
+      setText(newText.join(" "));
+    }
+    
+
+    const handleOnChange=(event)=>{
+        //console.log("onchange");
+        setText(event.target.value);
+    }
+
+   
+    const [text, setText] = useState("");
+
+
+  return (
+    <>
+  <div className="container  my-3" style={{color:props.mode==='dark'?'white':'black'}}>    
+   <h1>{props.heading}</h1>
+    <div className="mb-3">
+        <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows={8}
+            style={{backgroundColor:props.mode==='dark'?'grey':'white' ,color:props.mode==='dark'?'white':'black',border:'2px solid blue'}}
+            //style={{ border:'2px solid blue'}}
+            
+        />
+    </div>
+
+            <div className="container " >
+            <button className='btn btn-primary mx-3' onClick={handleUpClick}>convert to uppercase</button>
+            <button className='btn btn-primary mx-3' onClick={handleLowClick}>convert to lowerrcase</button>
+            <button className='btn btn-primary mx-3' onClick={handleClrClick}>cleare Text</button>
+            <button className='btn btn-primary mx-3' onClick={handleCopy}>copy Text</button>
+            <button className='btn btn-primary mx-3' onClick={handleExtraSpaces}>Remove extra spaces</button>
+            </div>   
+  </div>  
+
+  <div className="container my-2 " style={{color:props.mode==='dark'?'white':'#031d37'}}>
+    <h2>Your text summary</h2>
+    <p>{text.split(" ").length} words and {text.length} characters </p>
+    <p><b>{0.008 * text.split(" ").length} Minutes to read</b></p>
+    <h3>Preview</h3>
+    <p>{text}</p>
+  </div>
+  
+</>
+  )
+}
